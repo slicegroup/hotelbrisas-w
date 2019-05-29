f59a50eec5b03d904d6964e875c04bf09698dfce
navasg <genenavas97@gmail.com>
2019-05-29 15:15:59 -0400

    Add Home - Frontend

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+$('.single-banner').slick({
+  // arrow: true,
+  prevArrow: '<div class="slick-prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',//$('.slick-prev-arrow'),
+  nextArrow: '<div class="slick-next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',//$('.slick-next-arrow'),
+});
