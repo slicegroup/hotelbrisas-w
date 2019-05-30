ee9d57603126c06e74f24191d5d83d7b4f232498
navasg <genenavas97@gmail.com>
2019-05-30 12:25:51 -0400

    Add carousel services

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,20 @@ $('.single-banner').slick({
   prevArrow: '<div class="slick-prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',//$('.slick-prev-arrow'),
   nextArrow: '<div class="slick-next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',//$('.slick-next-arrow'),
 });
+
+$('.slick-room').slick({
+  prevArrow: '<div class="slick-prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',//$('.slick-prev-arrow'),
+  nextArrow: '<div class="slick-next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',//$('.slick-next-arrow'),
+});
+
+$('.slick-services').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  vertical: true,
+  verticalSwiping: true,
+  dots: true,
+  // autoplay: true,
+  // autoplaySpeed: 1000
+
+});
