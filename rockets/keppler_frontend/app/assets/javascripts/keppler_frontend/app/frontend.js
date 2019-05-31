ac6fb44e92588ea7812af5283ce790abf649b73d
Hugo Rivero <hugo200440@gmail.com>
2019-05-31 15:50:20 -0400

    update merge front and back

--- rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/app/frontend.js
+++ rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/app/frontend.js
@@ -1,2 +1,26 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$( document ).ready(function() {
+	$('.single-banner').slick({
+		// arrow: true,
+		prevArrow: '<div class="slick-prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',//$('.slick-prev-arrow'),|
+		nextArrow: '<div class="slick-next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',//$('.slick-next-arrow'),
+	});
+
+	$('.slick-room').slick({
+		prevArrow: '<div class="slick-prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',//$('.slick-prev-arrow'),
+		nextArrow: '<div class="slick-next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',//$('.slick-next-arrow'),
+	});
+
+	$('.slick-services').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		vertical: true,
+		verticalSwiping: true,
+		dots: true,
+		// autoplay: true,
+		// autoplaySpeed: 1000
+	});
+});
