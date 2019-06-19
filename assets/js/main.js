314ae320af31fbd9fc9ce651ea0ba1f84d9b2fac
navasg <genenavas97@gmail.com>
2019-06-18 22:57:34 -0400

    Responsive xs banner-rooms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $('.single-banner').slick({
-  // arrow: true,
-  prevArrow: '<div class="slick-prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',//$('.slick-prev-arrow'),
-  nextArrow: '<div class="slick-next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',//$('.slick-next-arrow'),
+  arrows: false,
+  // prevArrow: '<div class="slick-prev-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></div>',//$('.slick-prev-arrow'),
+  // nextArrow: '<div class="slick-next-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>',//$('.slick-next-arrow'),
 });
 
 $('.slick-room').slick({
